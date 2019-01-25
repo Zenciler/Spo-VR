73a18c87777135d96d83839b2bf18a855a8026f4
ybasesme <ybasesme@etu.edu.tr>
2019-01-25 17:07:54 +0300

    yeni sahne ve buton handler oluşturuldu

---
+++ Assets/Scripts/buttonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonBehaviour : MonoBehaviour
+{
+   public void getBicepsVideos()
+    {
+        Debug.Log("Biceps Videos Scene Will be bringed here");
+    }
+}
