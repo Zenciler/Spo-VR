02c17fd693d6dbd09e565d58be989667d0afaff4
Furkan Fatih Arslan <arslanfurkanfatih@gmail.com>
2019-02-16 16:43:55 +0300

    bu branch içeriği sonradan kullanılacak

---
+++ Assets/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
