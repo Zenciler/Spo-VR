99effa7b81149724ce5dcdba52ca1b2ab5230cab
ybasesme <ybasesme@etu.edu.tr>
2019-02-20 22:00:49 +0300

    çıkış butonu ve geri butonu çalışır hale geldi

---
+++ Assets/Scripts/UygulamadanCik.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UygulamadanCik : MonoBehaviour
+{
+   public void uygulamadanCik()
+    {
+        Application.Quit();
+    }
+
+}
